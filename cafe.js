bf4c833ced09c05afeb6cb1c593c0fec6bedcbbe
ariblet3 <Noobpker2233>
2017-11-27 19:17:56 -0600

    final_project_csc175

---
+++ cafe.js
@@ -0,0 +1,95 @@
+"use strict";
+
+$(document).ready(function() {
+    var total = 0;
+    
+    // add image swap and click handler to each image in table
+    $.getJSON("https://github.com/parkland-CSC175/parkland-CSC175.github.io/blob/master/content.json", function(data){
+        console.log(content.json);
+    $("ul img").each(function() {
+        var oldURL = $(this).attr("src"); // gets the src attribute
+        var newURL = $(this).attr("id"); // gets the id attribute
+        
+        // preload rollover image
+        var rolloverImage = new Image();
+        rolloverImage.src = newURL;
+        
+        // set up event handlers
+        $(this).hover(
+            function() {
+                $(this).attr("src", newURL); // sets the src attribute
+            },
+            function() {
+                $(this).attr("src", oldURL); // sets the src attribute
+            }
+        ); // end hover
+
+        $(this).click(function(evt) {
+            // get text from textarea
+            var order  = $("#order").html();
+            if (order == undefined) {
+            	order = "";
+            }
+            
+            // add selection to text and total
+            if (oldURL == "images/10lb_ABC_stored_pressure.jpg") {
+                total = total + 70.00;
+                order = order + '<option value="10lbABC">$70.00 - 10lb. ABC Stored Pressure</option>'; //set the pics to 117-120 pixels
+            } else if (oldURL == "images/BC_10lb_red_line_cartridge_operated.png") {
+                total = total + 400.00;
+                order = order + '<option value="cappuccino">400.00 - 10 lb. Red Line Cartridge Operated</option>';
+            } else if (oldURL == "images/class_D.png") {
+                total = total + 400.00;
+                order = order + '<option value="drip">$400.00 - Class D</option>';
+            } else if (oldURL == "images/10lb_CO2.jpg") {
+                total = total + 140.00;
+                order = order + '<option value="expresso">$140.00 - 10 lb. Co2</option>';
+            } else if (oldURL == "images/ABC_30lb_cart_op.jpg") {
+                total = total + 700.00;
+                order = order + '<option value="latte">$700 - ABC 30 lb. Cartridge Operated</option>';
+            } else if (oldURL == "images/K_class.jpg") {
+                total = total + 135.00;
+                order = order + '<option value="scone">$135.00 - K Class</option>';
+            }
+            else if (oldURL == "images/Bracket_10lb_HD_Red_Line.jpg") {
+                total = total + 104.00;
+                order = order + '<option value="scone">$104.00 - 10 lb HD Red Line Bracket</option>';
+            }
+            else if (oldURL == "images/20lb_cart_op_bracket.jpg") {
+                total = total + 92.50;
+                order = order + '<option value="scone">$92.50 - 20 lb Cartridge Operated Bracket</option>';
+            }
+            else if (oldURL == "images/Amerex_Cylinder_Galvanized_Bracket.jpg") {
+                total = total + 83.99;
+                order = order + '<option value="scone">$83.99 - 20 lb Amerex Cylinder Galvanized Brackett</option>';
+            }
+            
+            // display order and total
+            $("#order").html( order );
+            $("#total").text( "Total: $" + total.toFixed(2) );
+            
+            // cancel default event of the clicked link
+            evt.preventDefault();
+            
+        }); // end click
+    }); // end each
+}); //end json
+
+    // add click event handler for check out button
+    $("#place_order").click(function() {
+        var order = $("#order").text().trim();
+        if (order == "") {
+            alert ("Please add at least one item to your order.");
+        } else {
+            $("#order_form").submit();
+        }
+    }); // end click
+    
+    // add click event handler for clear button
+    $("#clear_order").click(function() {
+        total = 0;
+        $("#order").text( "" );
+        $("#total").text( "" );
+    }); // end click
+    
+}); // end ready
---
+++ contact.js
@@ -0,0 +1,126 @@
+function renderBlogs(){
+	
+	for(var i = 0; i < blogArticles.length; i++){
+		// pass each element in the array off to the 
+		// 'renderBlog' function
+		renderBlog(blogArticles[i]);		
+	}
+}
+
+function renderBlog(blog){
+	/*
+		<div class="article-wrapper">
+	
+			<h1>TITLE</h1>
+			<h4>AUTHOR</h4>
+			<h4>PUBLISHEDON</h4>
+	
+			<div class="content-wrapper">
+				<h2>HEADING</h2>
+				<p>PARAGRAPH</p>
+			</div>
+		</div>
+	*/
+	
+	var articleWrapper = document.createElement("div");
+	var title = document.createElement("h1");
+	var author = document.createElement("h4");
+	var publishedOn = document.createElement("h4");	
+	var wrapper = document.createElement("div");
+	
+	// set content
+	title.innerText = blog.title;
+	author.innerText = blog.author;
+	publishedOn.innerText = blog.publishedOn;
+	
+	// set classes
+	articleWrapper.setAttribute("class", "article-wrapper");
+	wrapper.setAttribute("class", "content-wrapper");
+	
+	// create hierarchy
+	articleWrapper.appendChild(title);
+	articleWrapper.appendChild(author);
+	articleWrapper.appendChild(publishedOn);
+	articleWrapper.appendChild(wrapper);
+	
+	var contents = blog.content;
+	
+	for(var i = 0; i < contents.length; i++){
+		var content = contents[i];
+		
+		/*
+			<div class="content-wrapper">
+				<h2>HEADING</h2>
+				<p>PARAGRAPH</p>
+			</div>
+		*/
+		
+		var heading = document.createElement("h2");
+		var parag = document.createElement("p");
+		
+		heading.innerText = content.heading;
+		parag.innerText = content.paragraph;
+		
+		if(content.heading && content.heading !== ""){
+			wrapper.appendChild(heading);
+		}
+		wrapper.appendChild(parag);
+	}
+	
+	var container = document.getElementById("blog-container");
+	container.appendChild(articleWrapper);	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// use this for part 2 of the assignment
+var blogArticles = [{
+		title: "Contact us for more information!",
+		author: "*Our phone number is, 1-800-2233",
+		publishedOn: "",
+		url: "http://www.paulirish.com/2015/advanced-performance-audits-with-devtools/",
+		content: [{
+				heading: "",
+				paragraph: "We Would love for you to come and visit!!"
+			},{
+				heading: "",
+				paragraph: "The environment of our store is clean, friendly, and most importantly cost effective!"
+			},{
+				heading: "",
+				paragraph: ""
+			}	
+		]
+	},{
+		title: "Send us an email.",
+		author: "'ariblet3@yahoo.com'",
+		publishedOn: "We would love to hear feedback! Also, if you could not find an item, we can order it for you!",
+		content: [{
+				heading: "Slow",
+				paragraph: ""
+			},{
+				heading: "",
+				paragraph: ""
+			},{
+				heading: "",
+				paragraph: ""
+			}
+		]	
+}];
+
+renderBlogs();
+
+var img = document.createElement("img");
+img.src = "house.jpg";
+
+var src = document.getElementById("container");
+src.appendChild(img);
